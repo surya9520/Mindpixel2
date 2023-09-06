5abc5508fc0375644aa9c2898991e763afc550ef
surya <suryajoshi617@gmail.com>
2023-09-06 15:07:50 +0530

    cors-updated

--- server/index.js
+++ server/index.js
@@ -30,7 +30,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
-app.use(cors());
+app.use(cors({
+  origin: 'https://mindpixel2-csil.vercel.app'
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 //*FILE STORAGE
