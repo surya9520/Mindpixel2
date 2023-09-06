815e134baaedf51001f785cc329f364db7216457
surya <suryajoshi617@gmail.com>
2023-09-06 15:18:58 +0530

    update

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors({
-  origin: 'https://mindpixel2-csil.vercel.app'
+  origin: 'https://mindpixel2.vercel.app/'
 }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
